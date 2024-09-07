7bd5ffb77fa2b1f793bd4b476d4cf27026a3d19a
Lucas Montenegro <lucasmontenegroburgos@gmail.com>
2024-09-07 20:15:28 -0300

    reinicio - parte 2

--- interactive.js
+++ interactive.js
@@ -1,52 +1,185 @@
-const datos = [];
+const carritoFisico = document.getElementById ('carrito')
+const misProductos = document.getElementById('productos')
+let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+let carritovaciado = document.getElementById('vaciar-carrito')
 
-// Lista definida - ORDENAR
+// Lista definida - ORDENADA
 const productos = [
-  { id: 1, nombre: "Rifle de francotirador Broughton M2000-D - 5 rondas", precio: 3500, colores: ["Negro", "Agrisado", "Blanco", "Ocre", "Lila"] },
-  { id: 2, nombre: "Pistola Glock 25 Radetec Slide c/ visor digital - 10 rondas", precio: 600, colores: ["Negro", "Agrisado", "Blanco", "Naranja", "Marrón"] },
-  { id: 3, nombre: "Subfusil FN P90 - 50 rondas", precio: 2000, colores: ["Negro", "Agrisado", "Blanco", "Turquesa", "Rosa"] },
-  { id: 4, nombre: "Kit convertor + FN Five-Seven - 20 rondas", precio: 3000, colores: ["Negro", "Agrisado", "Blanco", "Ocre", "Musgo"] },
-  { id: 5, nombre: "Carabina Corvo CAR-4 - 20 rondas", precio: 2200, colores: ["Negro", "Agrisado", "Blanco", "Ocre", "Greige"] },
-  { id: 6, nombre: "Subsubil TKA Cobra - 20 rondas", precio: 2200, colores: ["Negro", "Agrisado", "Blanco", "Ocre", "Marrón"] },
-  { id: 7, nombre: "Subfusil SG Compact-5 - 20 rondas", precio: 2300, colores: ["Negro", "Agrisado", "Blanco", "Ocre", "Marrón"] },
-  { id: 8, nombre: "Rifle táctico Swordfish pentaburst - 50 rondas", precio: 1700, colores: ["Negro", "Agrisado", "Blanco", "Marrón", "Musgo"] },
-  { id: 9, nombre: "Rifle de asalto TAQ-VF 7S - 30 rondas", precio: 1600, colores: ["Negro", "Agrisado", "Blanco", "Ocre", "Musgo"] },
-  { id: 10, nombre: "Revolver de acción simple Corvo DS6 duales - 5 rondas", precio: 800, colores: ["Negro", "Agrisado", "Blanco", "Marrón", "Púrpura"] },
-  { id: 11, nombre: "Rifle de francotirador AIM 90 - 10 rondas", precio: 3200, colores: ["Negro", "Agrisado", "Blanco", "Marrón", "Oliva"] },
-  { id: 12, nombre: "Fusil de asalto AK Surorov SV7 - 10 rondas", precio: 1400, colores: ["Negro", "Agrisado", "Dorado", "Ocre", "Marrón"] },
-  { id: 13, nombre: "Rifle de francotirador AK-R - 30 rondas", precio: 3000, colores: ["Negro", "Agrisado", "Dorado", "Ocre", "Marrón"] },
-  { id: 14, nombre: "Escopeta CG PS12G - 5 rondas", precio: 1800, colores: ["Negro", "Agrisado", "Blanco", "Ocre", "Marrón"] },
-  { id: 15, nombre: "Escopeta Ferino SP-14 - 10 rondas", precio: 2200, colores: ["Negro", "Agrisado", "Blanco", "Marrón", "Greige"] },
-  { id: 16, nombre: "Pistola HJKE-11 Yukimura - variante Skippy - 30 rondas", precio: 4200, colores: ["Dorado", "Agrisado", "Verde", "Púrpura", "Blanco"] },
-  { id: 17, nombre: "Escopeta Carnage - 10 rondas", precio: 6000, colores: ["Negro", "Amarillo", "Rojo", "Marrón", "Verde"] },
-  { id: 18, nombre: "Escopeta CN/ST-23 - 20 rondas", precio: 4200, colores: ["Negro", "Agrisado", "Blanco", "Ocre", "Marrón"] },
-  { id: 19, nombre: "Rifle de francotirador Eaton modelo 8 - 10 rondas", precio: 3000, colores: ["Negro", "Agrisado", "Greige", "Ocre", "Oliva"] },
-  { id: 20, nombre: "Fusil de tirador SA M308 - 10 rondas", precio: 1600, colores: ["Negro", "Agrisado", "Blanco", "Marrón", "Greige"] },
-  { id: 21, nombre: "Rifle de asalto AK-102 - 30 rondas", precio: 1400, colores: ["Negro", "Greige", "Oliva", "Ocre", "Lila"] },
-  { id: 22, nombre: "Rifle de asalto bullup Tavor x95 - 30 rondas", precio: 1600, colores: ["Negro", "Agrisado", "Blanco", "Oliva", "Greige"] },
-  { id: 23, nombre: "Escopeta Lockwood 680 - 7 rondas", precio: 1800, colores: ["Negro", "Agrisado", "Blanco", "Oliva", "Greige"] },
-  { id: 24, nombre: "Rifle de francotirador M21 camuflado - 10 rondas", precio: 3500, colores: ["Negro", "Agrisado", "Blanco", "Ocre", "Musgo"] },
-  { id: 25, nombre: "Escopeta M2038 táctica - 10 rondas", precio: 7000, colores: ["Negro", "Marrón", "Greige", "Oliva", "Dorado"] },
+  { id: 1, nombre: "Rifle de francotirador Broughton M2000-D - 5 rondas", precio: 3500, colores: ["Negro", "Agrisado", "Blanco", "Ocre", "Lila"], imagen: "e-commerce/producto-uno.png" },
+  { id: 2, nombre: "Pistola Glock 25 Radetec Slide c/ visor digital - 10 rondas", precio: 600, colores: ["Negro", "Agrisado", "Blanco", "Naranja", "Marrón"], imagen: "e-commerce/producto-dos.png" },
+  { id: 3, nombre: "Subfusil FN P90 - 50 rondas", precio: 2000, colores: ["Negro", "Agrisado", "Blanco", "Turquesa", "Rosa"], imagen: "e-commerce/producto-tres.png" },
+  { id: 4, nombre: "Kit convertor + FN Five-Seven - 20 rondas", precio: 3000, colores: ["Negro", "Agrisado", "Blanco", "Ocre", "Musgo"], imagen: "e-commerce/producto-cuatro.png" },
+  { id: 5, nombre: "Carabina Corvo CAR-4 - 20 rondas", precio: 2200, colores: ["Negro", "Agrisado", "Blanco", "Ocre", "Greige"], imagen: "e-commerce/producto-cinco.png" },
+  { id: 6, nombre: "Subsubil TKA Cobra - 20 rondas", precio: 2200, colores: ["Negro", "Agrisado", "Blanco", "Ocre", "Marrón"], imagen: "e-commerce/producto-seis.png" },
+  { id: 7, nombre: "Subfusil SG Compact-5 - 20 rondas", precio: 2300, colores: ["Negro", "Agrisado", "Blanco", "Ocre", "Marrón"], imagen: "e-commerce/producto-siete.png" },
+  { id: 8, nombre: "Rifle táctico Swordfish pentaburst - 50 rondas", precio: 1700, colores: ["Negro", "Agrisado", "Blanco", "Marrón", "Musgo"], imagen: "e-commerce/producto-ocho.png" },
+  { id: 9, nombre: "Rifle de asalto TAQ-VF 7S - 30 rondas", precio: 1600, colores: ["Negro", "Agrisado", "Blanco", "Ocre", "Musgo"], imagen: "e-commerce/producto-nueve.png" },
+  { id: 10, nombre: "Revolver de acción simple Corvo DS6 duales - 5 rondas", precio: 800, colores: ["Negro", "Agrisado", "Blanco", "Marrón", "Púrpura"], imagen: "e-commerce/producto-diez.png" },
+  { id: 11, nombre: "Rifle de francotirador AIM 90 - 10 rondas", precio: 3200, colores: ["Negro", "Agrisado", "Blanco", "Marrón", "Oliva"], imagen: "e-commerce/producto-once.png" },
+  { id: 12, nombre: "Fusil de asalto AK Surorov SV7 - 10 rondas", precio: 1400, colores: ["Negro", "Agrisado", "Dorado", "Ocre", "Marrón"], imagen: "e-commerce/producto-doce.png" },
+  { id: 13, nombre: "Rifle de francotirador AK-R - 30 rondas", precio: 3000, colores: ["Negro", "Agrisado", "Dorado", "Ocre", "Marrón"], imagen: "e-commerce/producto-trece.png" },
+  { id: 14, nombre: "Escopeta CG PS12G - 5 rondas", precio: 1800, colores: ["Negro", "Agrisado", "Blanco", "Ocre", "Marrón"], imagen: "e-commerce/producto-catorce.png" },
+  { id: 15, nombre: "Escopeta Ferino SP-14 - 10 rondas", precio: 2200, colores: ["Negro", "Agrisado", "Blanco", "Marrón", "Greige"], imagen: "e-commerce/producto-quince.png" },
+  { id: 16, nombre: "Pistola HJKE-11 Yukimura - variante Skippy - 30 rondas", precio: 4200, colores: ["Dorado", "Agrisado", "Verde", "Púrpura", "Blanco"], imagen: "e-commerce/producto-dieciseis.png" },
+  { id: 17, nombre: "Escopeta Carnage - 10 rondas", precio: 6000, colores: ["Negro", "Amarillo", "Rojo", "Marrón", "Verde"], imagen: "e-commerce/producto-diecisiete.png" },
+  { id: 18, nombre: "Escopeta CN/ST-23 - 20 rondas", precio: 4200, colores: ["Negro", "Agrisado", "Blanco", "Ocre", "Marrón"], imagen: "e-commerce/producto-dieciocho.png" },
+  { id: 19, nombre: "Rifle de francotirador Eaton modelo 8 - 10 rondas", precio: 3000, colores: ["Negro", "Agrisado", "Greige", "Ocre", "Oliva"], imagen: "e-commerce/producto-diecinueve.png" },
+  { id: 20, nombre: "Fusil de tirador SA M308 - 10 rondas", precio: 1600, colores: ["Negro", "Agrisado", "Blanco", "Marrón", "Greige"], imagen: "e-commerce/producto-veinte.png" },
+  { id: 21, nombre: "Rifle de asalto AK-102 - 30 rondas", precio: 1400, colores: ["Negro", "Greige", "Oliva", "Ocre", "Lila"], imagen: "e-commerce/producto-veintiuno.png" },
+  { id: 22, nombre: "Rifle de asalto bullup Tavor x95 - 30 rondas", precio: 1600, colores: ["Negro", "Agrisado", "Blanco", "Oliva", "Greige"], imagen: "e-commerce/producto-veintidos.png" },
+  { id: 23, nombre: "Escopeta Lockwood 680 - 7 rondas", precio: 1800, colores: ["Negro", "Agrisado", "Blanco", "Oliva", "Greige"], imagen: "e-commerce/producto-veintitres.png" },
+  { id: 24, nombre: "Rifle de francotirador M21 camuflado - 10 rondas", precio: 3500, colores: ["Negro", "Agrisado", "Blanco", "Ocre", "Musgo"], imagen: "e-commerce/producto-veinticuatro.png" },
+  { id: 25, nombre: "Escopeta M2038 táctica - 10 rondas", precio: 7000, colores: ["Negro", "Marrón", "Greige", "Oliva", "Dorado"], imagen: "e-commerce/producto-veinticinco.png" },
 ];
 
-// Variable global para almacenar el total acumulado y verificar acceso
+productos.forEach(el => {
+  const productosCards = generadoraDeCards(el.nombre, el.precio, el.imagen)
+  misProductos.appendChild(productosCards)
+})
+
+function generadoraDeCards (nombre, precio, imagen){
+  const contenedor = document.createElement('div')
+  const tituloDOM = document.createElement('p')
+  const imgDOM = document.createElement('img')
+  const precioDOM = document.createElement("p")
+  const botonAgregarAlCarrito = document.createElement('button')
+  const botonSumarUnidad = document.createElement('button')
+  const botonRestarUnidad = document.createElement('button')
+
+  contenedor.classList.add('contenedorDeCard')
+  tituloDOM.classList.add('tituloCard')
+  precioDOM.classList.add('tituloCard')
+  botonSumarUnidad.classList.add('boton')
+  botonRestarUnidad.classList.add('boton')
+  botonAgregarAlCarrito.addEventListener('click', ( ) =>{
+    agregarAlCarrito (nombre, precio), mostrarAlertDeConfirmacion (nombre)
+  })
+  botonSumarUnidad.addEventListener('click', ( ) =>{
+    sumarUnidad (nombre)
+  })
+  botonRestarUnidad.addEventListener('click', ( ) =>{
+    restarUnidad (nombre)
+  })
+
+  tituloDOM.innerText = nombre
+  precioDOM.innerText = `$${precio}`
+  imgDOM.src = imagen
+  botonAgregarAlCarrito.innerText = 'agregar al carrito'
+  botonSumarUnidad.innerText = '+'
+  botonRestarUnidad.innerText = '-'
+
+  contenedor.appendChild(tituloDOM)
+  contenedor.appendChild(imgDOM)
+  contenedor.appendChild(precioDOM)
+  contenedor.appendChild(botonAgregarAlCarrito)
+  contenedor.appendChild(botonSumarUnidad)
+  contenedor.appendChild(botonRestarUnidad)
+  return contenedor
+}
+
+// Función para agregar un producto al carrito
+function agregarAlCarrito (nombre, precio){
+    
+  const verificador = carrito.some(el => {
+    return el.nombre === nombre
+   })
+   if(verificador){
+const producto = carrito.find(el => {
+    return el.nombre === nombre
+})
+producto.cantidad += 1
+   }
+
+   else{
+    carrito.push({
+        nombre: nombre,
+        precio: precio,
+        cantidad: 1
+    })
+   }   
+}
+
+// Función para sumar un producto al carrito
+function sumarUnidad(nombre) {
+  const producto = carrito.find(el => el.nombre === nombre);
+  if (producto) {
+      producto.cantidad += 1;
+  }
+  actualizarCarrito();
+}
+
+// Función para restar un producto al carrito
+function restarUnidad(nombre) {
+  const producto = carrito.find(el => el.nombre === nombre);
+
+  if (producto.cantidad <= 1) {
+      const index = carrito.findIndex(el => el.nombre === nombre);
+      carrito.splice(index, 1);
+  } else {
+      producto.cantidad -= 1;
+  }
+
+  actualizarCarrito();
+}
+
+// Función para actualizar el carrito
+function actualizarCarrito (){
+  const totalAPagar = document.createElement('p')
+totalAPagar.classList.add('boton')
+  carrito.innerHTML = '' 
+  const totalProductos = carrito.reduce((acc, el) =>{
+      return acc + el.cantidad * el.precio
+  }, 0)
+  totalAPagar.innerText = totalProductos
+  carrito.forEach(el => {
+      carritoFisico.appendChild(totalAPagar)
+      localStorage.setItem("carrito", JSON.stringify(carrito))
+  })
+
+}
+
+// Función para confirmar el producto
+function mostrarAlertDeConfirmacion (nombre){
+  Swal.fire({
+     title: `${nombre} fue agregada al carrito`,
+     icon: 'success',
+     showConfirmButton: false,
+     position: "top",
+     timer: 3000,
+   })
+ 
+}
+
+// Función para ver el total del carrito
+
+
+// Función para limpiar el carrito
+//function limpiarCarrito (){
+//  localStorage.clear()
+//}
+carritovaciado.addEventListener('click', () => {
+  localStorage.clear()
+}
+)
+
+/*// Variable global para almacenar el total acumulado y verificar acceso
 let totalCarrito = 0;
 let usuarioAccedido = false;
 
 // Función para mostrar los productos
 function mostrarProductos() {
-  let mensaje = "Nuestro catálogo:<br>";
+  let mensaje = "Nuestro catálogo:";
   productos.forEach(producto => {
-    mensaje += `${producto.id}. ${producto.nombre} - $${producto.precio}<br>`;
+    mensaje += `${producto.id}. ${producto.nombre} - $${producto.precio}`;
   });
   document.getElementById('productos').innerHTML = mensaje;
 }
 
 // Función para mostrar los colores disponibles
 function mostrarColores(producto) {
-  let mensaje = `Colores disponibles para ${producto.nombre}<br>`;
+  let mensaje = `Colores disponibles para ${producto.nombre}`;
   producto.colores.forEach((color, index) => {
-    mensaje += `${index + 1}. ${color}<br>`;
+    mensaje += `${index + 1}. ${color}`;
   });
   return mensaje;
 }
@@ -218,16 +351,6 @@ function mostrarTotalCarrito() {
 
 document.getElementById('show-cart').addEventListener('click', mostrarTotalCarrito);
 
-// Función para agregar un producto al carrito
-function addToCart(product) {
-  if (!usuarioAccedido) {
-    alert("Debés ingresar tus datos para continuar.");
-    return;
-  }
-
-  mostrarYComprarProductos();
-}
-
 // Función para vaciar el carrito
 function clearCart() {
   document.querySelector('.cart-items').innerHTML = '';
@@ -247,7 +370,7 @@ document.getElementById('clear-cart').disabled = true;
 
 // Cerrar el modal de bienvenida
 document.getElementById('close-modal').addEventListener('click', () => {
-  document.getElementById('welcome-modal').style.display = 'none';
+document.getElementById('welcome-modal').style.display = 'none';
 });
 
 // Habilitar botones y guardar datos de usuario al hacer clic en "Ingresar"
@@ -279,13 +402,13 @@ document.getElementById('confirmar-compra').addEventListener('click', () => {
   }
 
   // Lógica para agregar productos al carrito
-  agregarAlCarrito(productoId, colorId, cantidad);
+  agregarAlCarrito(nombre, color, cantidad);
 });
 
-// Función para agregar productos al carrito (ejemplo)
-function agregarAlCarrito(productoId, colorId, cantidad) {
+// Función para agregar productos al carrito
+function agregarAlCarrito(nombre, color, cantidad) {
   const carrito = obtenerCarrito();
-  carrito.push({ productoId, colorId, cantidad });
+  carrito.push({ nombre, color, cantidad });
   localStorage.setItem('carrito', JSON.stringify(carrito));
   mostrarCarrito();
 }
@@ -311,3 +434,4 @@ document.getElementById('clear-cart').addEventListener('click', () => {
 
 // Mostrar carrito al hacer clic en el botón
 document.getElementById('show-cart').addEventListener('click', mostrarCarrito);
+*/
